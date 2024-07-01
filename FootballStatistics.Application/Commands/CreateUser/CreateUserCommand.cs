c558082e541c99151e80c0c6fd12362da855e193
Luiz Alberto <Luiz.albuquerque99@gmail.com>
2024-07-01 16:37:44 -0300

    feat: implement application layer

---
+++ FootballStatistics.Application/Commands/CreateUser/CreateUserCommand.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballStatistics.Application.Commands.CreateUser
+{
+    public class CreateUserCommand : IRequest<int>
+    {
+        public CreateUserCommand(string name, string email, string password)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+        }
+
+        public string Name { get; set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+    }
+}

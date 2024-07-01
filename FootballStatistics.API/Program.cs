874fb8026f2020176e920748691242d857cb4031
Luiz Alberto <Luiz.albuquerque99@gmail.com>
2024-07-01 16:51:57 -0300

    feat: mediator configuration

--- FootballStatistics.API/Program.cs
+++ FootballStatistics.API/Program.cs
@@ -1,6 +1,11 @@
+using FootballStatistics.Application.Commands.CreateUser;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddMediatR(configuration => configuration.RegisterServicesFromAssemblyContaining<CreateUserCommand>());
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle

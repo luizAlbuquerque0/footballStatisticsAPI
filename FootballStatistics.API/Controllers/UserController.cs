6385f536b1543a0e6ff08c0852fcb0b375374f89
Luiz Alberto <Luiz.albuquerque99@gmail.com>
2024-07-01 16:43:29 -0300

    feat: implement user controller

---
+++ FootballStatistics.API/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using FootballStatistics.Application.Commands.CreateUser;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FootballStatistics.API.Controllers
+{
+    [Route("api/users")]
+    public class UserController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public UserController(IMediator mediator) 
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser([FromBody] CreateUserCommand command)
+        {
+            var id = _mediator.Send(command);
+
+            return CreatedAtAction(nameof(GetById), new {id = id}, command);
+        }
+    }
+}

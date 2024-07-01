d9a2288745361d93800d27773242c39942225f67
Luiz Alberto Albuquerque <luiz.albuquerque99@gmail.com>
2024-07-01 16:33:11 -0300

    feat: implement infrastructure layer

---
+++ FootballStatistics.Infrastructure/Configurations/UserConfigurations.cs
@@ -0,0 +1,15 @@
+﻿using FootballStatistics.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FootballStatistics.Infrastructure.Configurations
+{
+    public class UserConfigurations : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder
+                .HasKey(x => x.Id);
+        }
+    }
+}
---
+++ FootballStatistics.Infrastructure/Persistence/FootballStatisticsDbContext.cs
@@ -0,0 +1,19 @@
+﻿using FootballStatistics.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballStatistics.Infrastructure.Persistence
+{
+    public class FootballStatisticsDbContext : DbContext
+    {
+        public FootballStatisticsDbContext(DbContextOptions<FootballStatisticsDbContext> options) : base(options)
+        { 
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
